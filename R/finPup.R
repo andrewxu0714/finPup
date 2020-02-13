c1adb4e51fed15045f575f39f7df37efe2a64ffb
andrewxu0714 <40601697+andrewxu0714@users.noreply.github.com>
2020-02-13 16:28:27 +1100

    Add files via upload

---
+++ R/finPup.R
@@ -0,0 +1,16 @@
+apikey<-'15243673-384387da3fd1e8a9618d1153b'
+
+finPup <- function(type="popular",page=1)
+{
+  if (!(type %in% c('popular','latest'))){
+    type='popular'
+  }
+  pups <- content(GET(paste0("https://pixabay.com/api/?key=",apikey,"&q=dog&image_type=photo&per_page=200&order=",type,"&page=",page)))
+  pupselect <- round(runif(1,1,length(pups$hits)))
+  puplocate <- pups$hits[[pupselect]]$largeImageURL
+  puphouse <- paste0(tempdir(), "/doggo.png")
+  download.file(puplocate, puphouse,
+                mode = "wb")
+  pupper <- load.image(puphouse)
+  plot(pupper, yaxt = "n", axes = FALSE)
+}
