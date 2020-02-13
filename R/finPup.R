9323e1c5112b5d0da73287f4ccf9c5cb12b686f8
andrewxu0714 <40601697+andrewxu0714@users.noreply.github.com>
2020-02-13 16:39:36 +1100

    Add files via upload

--- R/finPup.R
+++ R/finPup.R
@@ -1,3 +1,8 @@
+library(rvest)
+library(httr)
+library(jsonlite)
+library(imager)
+
 apikey<-'15243673-384387da3fd1e8a9618d1153b'
 
 finPup <- function(type="popular",page=1)
@@ -5,12 +10,12 @@ finPup <- function(type="popular",page=1)
   if (!(type %in% c('popular','latest'))){
     type='popular'
   }
-  pups <- content(GET(paste0("https://pixabay.com/api/?key=",apikey,"&q=dog&image_type=photo&per_page=200&order=",type,"&page=",page)))
+  pups <- httr::content(GET(paste0("https://pixabay.com/api/?key=",apikey,"&q=dog&image_type=photo&per_page=200&order=",type,"&page=",page)))
   pupselect <- round(runif(1,1,length(pups$hits)))
   puplocate <- pups$hits[[pupselect]]$largeImageURL
   puphouse <- paste0(tempdir(), "/doggo.png")
   download.file(puplocate, puphouse,
                 mode = "wb")
-  pupper <- load.image(puphouse)
+  pupper <- imager::load.image(puphouse)
   plot(pupper, yaxt = "n", axes = FALSE)
 }
